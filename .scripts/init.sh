10b7e5f0ed422dd5fd2b1b0502cca79e623a5d9b
Juan <jcuzmar@protonmail.com>
2020-09-02 20:52:05 -0400

    new

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
-~/.screenlayout/home.sh &
-(sleep 1 && nitrogen --restore) &
+nitrogen --restore &
 (sleep 1 && picom -b) &
 (sleep 1 && pulseaudio -k) &
 #(sleep 1 && trayer --edge bottom --align right --SetDockType true --SetPartialStrut true --expand true --width 6 --height 18 --transparent true --alpha 0 --tint 0x000000 --expand true --iconspacing 2 --margin 4 --distance 4) &
